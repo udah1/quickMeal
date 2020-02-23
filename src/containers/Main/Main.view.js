6c7ac1e3c5b1174773ad634ccc00bcf4f1bc3d1a
Yehuda Huri <yehuda.huri@amdocs.com>
2020-02-23 13:44:29 +0200

    add food json

--- src/containers/Main/Main.view.js
+++ src/containers/Main/Main.view.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import Board from '../Board/Board.view'
-import {connect}    from 'react-redux'
+import {connect} from 'react-redux'
 
 class Main extends Component {
+    constructor(props) {
+        super();
+        this.foods = require('../../assets/foods.json');
+    }
 
     render() {
         return (
             <div>
                 <p /><p /><p /><p /><p /><p />
-                <Board />
+                <Board foods={this.foods}/>
             </div>
         );
     }
