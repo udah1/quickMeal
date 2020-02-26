f43ef1f8f022b433a0de5a5ab2cd512934de2680
Yehuda Huri <yehuda.huri@amdocs.com>
2020-02-26 17:03:17 +0200

    menu adter cart push

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
-import { combineReducers } from 'redux'
+import {combineReducers} from 'redux'
 
 import board from '../containers/Board/Board.reducer'
 import cube from '../containers/Cube/Cube.reducer'
-import main from '../containers/Main/Main.reducer'
+import menu from '../containers/Menu/Menu.reducer'
 import cart from '../containers/Cart/Cart.reducer'
 
 const rootReducer = combineReducers({
   reducer : combineReducers({ //nesting reducers...
       board,
       cube,
-      main,
+      menu,
       cart
   })
   //place other reducers and nested reducers here...
